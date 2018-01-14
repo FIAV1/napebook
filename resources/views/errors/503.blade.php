a5236d1df54ca4694e54bf8001c48c9ee04d7a29
NicFontana <niccolo.fontana@student.unife.it>
2018-01-14 12:53:12 +0100

    Corretta la view 503.blade.php che mostrava i commenti al codice

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,17 +1,18 @@
-/*
+{{--
 The parent errors::layout comes from inside the framework
 (/vendor/laravel/framework/src/Illuminate/Foundation/Exceptions/views/layout.blade.php)
 and you are welcome to continue using it, or create your layout file and extend it.
-*/
+--}}
+
 @extends('errors::layout')
 
 @section('title', 'Service Unavailable')
 
-/*
+{{--
 The method renderHttpException(HttpException $e)
 of the /vendor/laravel/framework/src/Illuminate/Foundation/Exceptions/Handler.php class
 pass the HttpException $e object to our view:
 "return response()->view($view, ['exception' => $e], $status, $e->getHeaders());"
 So we can use $exception to display the message we pass to 'php artisan down --message "..."'
-*/
+--}}
 @section('message', $exception->getMessage())
