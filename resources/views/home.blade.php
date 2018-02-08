fb39c731bb38dc87b8d6c98d1578c22ea3991ecd
NicFontana <niccolo.fontana@student.unife.it>
2018-02-08 15:06:00 +0100

    Fix volante al template home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,30 +2,32 @@
 
 @section('content')
 
-    <section id="session-message" class="my-2">
-        @if (session('status'))
-            <div class="container">
-                <div class="row">
-                    <div class="col-12">
-                        <div class="alert alert-success alert-dismissible fade show text-center" role="alert">
-                            {{ session('status') }}
-                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
+    @if (session('status'))
+        <section id="session-message" class="my-2">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="alert alert-success alert-dismissible fade show text-center" role="alert">
+                                {{ session('status') }}
+                                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        @endif
-    </section>
+        </section>
+    @endif
 
-    @include('create')
+    @include('post.create')
 
     <section id="posts">
         <div class="container">
-        @foreach ($posts as $post)
-            @include('post.post')
-        @endforeach
+            @foreach ($posts as $post)
+                @include('post.post')
+            @endforeach
+        </div>
+    </section>
 
     @include('layouts.error')
 
